aa472ef00cb59eee05d2e26c37f8c3e218da0a33
Chris <ceochrism@mail.com>
2021-09-02 18:30:34 -0700

    Created the line of best fit generator using hill climbing.  Using win forms graphics to display the line of best fit

---
+++ HillClimber/HillClimberLineBestFit/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace HillClimberLineBestFit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.XTextBox = new System.Windows.Forms.TextBox();
+            this.YTextBox = new System.Windows.Forms.TextBox();
+            this.AddPoint = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.CalculateBtn = new System.Windows.Forms.Button();
+            this.Equation = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PointsLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(259, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(815, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(815, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Y:";
+            // 
+            // XTextBox
+            // 
+            this.XTextBox.Location = new System.Drawing.Point(877, 37);
+            this.XTextBox.Name = "XTextBox";
+            this.XTextBox.Size = new System.Drawing.Size(100, 26);
+            this.XTextBox.TabIndex = 3;
+            // 
+            // YTextBox
+            // 
+            this.YTextBox.Location = new System.Drawing.Point(877, 80);
+            this.YTextBox.Name = "YTextBox";
+            this.YTextBox.Size = new System.Drawing.Size(100, 26);
+            this.YTextBox.TabIndex = 4;
+            // 
+            // AddPoint
+            // 
+            this.AddPoint.Location = new System.Drawing.Point(873, 137);
+            this.AddPoint.Name = "AddPoint";
+            this.AddPoint.Size = new System.Drawing.Size(104, 53);
+            this.AddPoint.TabIndex = 5;
+            this.AddPoint.Text = "Add Point";
+            this.AddPoint.UseVisualStyleBackColor = true;
+            this.AddPoint.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(873, 246);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(104, 55);
+            this.Clear.TabIndex = 6;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // CalculateBtn
+            // 
+            this.CalculateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalculateBtn.Location = new System.Drawing.Point(810, 344);
+            this.CalculateBtn.Name = "CalculateBtn";
+            this.CalculateBtn.Size = new System.Drawing.Size(181, 97);
+            this.CalculateBtn.TabIndex = 7;
+            this.CalculateBtn.Text = "Calculate Equation";
+            this.CalculateBtn.UseVisualStyleBackColor = true;
+            this.CalculateBtn.Click += new System.EventHandler(this.CalculateBtn_Click);
+            // 
+            // Equation
+            // 
+            this.Equation.AutoSize = true;
+            this.Equation.Location = new System.Drawing.Point(12, 97);
+            this.Equation.Name = "Equation";
+            this.Equation.Size = new System.Drawing.Size(77, 20);
+            this.Equation.TabIndex = 8;
+            this.Equation.Text = "Equation:";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.PointsLabel);
+            this.panel1.Location = new System.Drawing.Point(28, 285);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(193, 198);
+            this.panel1.TabIndex = 9;
+            // 
+            // PointsLabel
+            // 
+            this.PointsLabel.AutoSize = true;
+            this.PointsLabel.Location = new System.Drawing.Point(10, 19);
+            this.PointsLabel.Name = "PointsLabel";
+            this.PointsLabel.Size = new System.Drawing.Size(53, 20);
+            this.PointsLabel.TabIndex = 0;
+            this.PointsLabel.Text = "Points";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(300, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(413, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "(Graph Only Accurate if All Points Between -250 and 250)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 608);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Equation);
+            this.Controls.Add(this.CalculateBtn);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.AddPoint);
+            this.Controls.Add(this.YTextBox);
+            this.Controls.Add(this.XTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox XTextBox;
+        private System.Windows.Forms.TextBox YTextBox;
+        private System.Windows.Forms.Button AddPoint;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button CalculateBtn;
+        private System.Windows.Forms.Label Equation;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label PointsLabel;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ HillClimber/HillClimberLineBestFit/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HillClimberLineBestFit
+{
+    public partial class Form1 : Form
+    {
+        List<Point> points = new List<Point>();
+        Graphics gfx;
+        Bitmap canvas;
+        double slope = 0;
+        double yIntercept = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DrawAxis()
+        {
+            gfx.DrawLine(Pens.Black, canvas.Width / 2, 0, canvas.Width / 2, canvas.Height);
+            gfx.DrawLine(Pens.Black, 0, canvas.Height / 2, canvas.Width, canvas.Height / 2);
+        }
+
+        public void DrawPoints()
+        {
+            foreach (var point in points)
+            {
+                gfx.FillEllipse(Brushes.Red, point.X - 1 + canvas.Width / 2, -point.Y - 1 + canvas.Height / 2, 2, 2);
+            }
+        }
+
+        public void DrawLine()
+        {
+            //gfx.DrawLine(new Pen(Color.Green, 1), 0, (int)(-(canvas.Width / 2 * slope) - yIntercept), canvas.Width / 2, (int)(canvas.Height / 2 - yIntercept));
+            gfx.DrawLine(new Pen(Color.Green, 1), 0, -(int)(slope * (-(canvas.Width) / 2))-(int)yIntercept+canvas.Height/2, canvas.Width, -(int)(slope * ((canvas.Width)/2))+canvas.Height/2-(int)yIntercept); 
+        }
+
+        public void Update()
+        {
+            gfx.Clear(Color.White);
+            DisplayPoints();
+            DrawAxis();
+            DrawPoints();
+            DrawLine();
+            pictureBox1.Image = canvas;
+        }
+
+        public void DisplayPoints()
+        {
+            PointsLabel.Text = "Points: ";
+            foreach (var point in points)
+            {
+                PointsLabel.Text += $"\nX: {point.X} Y {point.Y}";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            points.Clear();
+            canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gfx = Graphics.FromImage(canvas);
+            Update();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(XTextBox.Text, out int x) && int.TryParse(YTextBox.Text, out int y))
+            {
+                points.Add(new Point(x, y));
+                XTextBox.Clear();
+                YTextBox.Clear();
+                Update();
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            DisplayPoints();
+        }
+
+        private void CalculateBtn_Click(object sender, EventArgs e)
+        {
+            double ErrorThreshold = 0;
+            foreach (var point in points)
+            {
+                ErrorThreshold += point.Y;
+            }
+            ErrorThreshold = Math.Abs(ErrorThreshold / 1);
+            double currentError = double.MaxValue;
+            double[] currentMutation = new double[2];
+            int timesSinceErrorChange = 0;
+            while (currentError > ErrorThreshold)
+            {
+                timesSinceErrorChange = 0;
+                currentMutation = new double[2];
+                currentError = CalculateError(currentMutation[0], currentMutation[1]);
+                while (timesSinceErrorChange < 25)
+                {
+                    currentMutation = Mutate(currentMutation, currentError);
+                    double newError = CalculateError(currentMutation[0], currentMutation[1]);
+                    if (newError < currentError)
+                    {
+                        timesSinceErrorChange = 0;
+                        currentError = newError;
+                    }
+                    else
+                    {
+                        timesSinceErrorChange++;
+                    }
+                }
+            }
+
+            slope = currentMutation[0];
+            yIntercept = currentMutation[1];
+            Equation.Text = $"Equation: y = {slope}x + {yIntercept}";
+
+            Update();
+
+        }
+
+        Random gen = new Random();
+        public double[] Mutate(double[] current, double currentError)
+        {
+
+            int index = gen.Next(0, current.Length);
+
+            double amount = .1;
+            if (gen.Next(0, 2) == 1)
+            {
+                amount = -1;
+            }
+            current[index] += amount;
+            current[index] = Math.Round(current[index], 2);
+            double newError = CalculateError(current[0], current[1]);
+            if (newError < currentError)
+            {
+                return current;
+            }
+            current[index] -= amount;
+            current[index] = Math.Round(current[index], 2);
+            return current;
+        }
+
+        public double CalculateError(double slope, double yIntercept)
+        {
+            double totalError = 0;
+            foreach (var point in points)
+            {
+                totalError += Math.Abs(point.Y - (slope * point.X + yIntercept));
+            }
+            return totalError / points.Count;
+        }
+    }
+}
